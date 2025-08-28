ab0d5ac6a56dfde2480690f133806803c663e3bd
Wes Jourdan <wes.jourdan@icloud.com>
2025-08-28 09:58:36 -0700

    Initial import (secrets removed)

---
+++ server/config/secrets.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    sessionSecret:    "xxxxxx"
+    , mandrill:       "zzzzzzzzzz"
+    , mongo_user:     "n/a"
+    , mongo_pass: "n/a"
+    , openai_key:   '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'
+  }
+
+  , staging: {
+    sessionSecret:    "xxxxxx"
+    , mandrill:       "zzzzzzzzzz"
+    , mongo_user:     "n/a"
+    , mongo_pass:     "n/a"
+  }
+
+  , production: {
+    sessionSecret:    "xxxxxx"
+    , mandrill:       "zzzzzzzzzz"
+    , mongo_user:     "n/a"
+    , mongo_pass:     "n/a"
+  }
+}
